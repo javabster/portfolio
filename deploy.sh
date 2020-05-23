5d861e716524c65204df6e70d128acfb8492c460
Abby Mitchell <abby.mitchell@btinternet.com>
2020-05-23 17:33:54 +0100

    Attempt deploy to aws

--- deploy.sh
+++ deploy.sh
@@ -29,7 +29,7 @@
 # fi
 
 AWS_S3_REGION="eu-west-2"
-CLOUDFRONT_DIST_ID=$CLOUDFRONT_DIST_ID_STAGING
+# CLOUDFRONT_DIST_ID=$CLOUDFRONT_DIST_ID_STAGING
 # the command that builds your app
 cd ./client
 npm run build
@@ -50,6 +50,4 @@ aws s3 sync public/ "s3://$S3_BUCKET" --acl public-read --delete
 
 # Force-invalidate the now-outdated assets rather than waiting for them to expire
 # Make sure you have the CLOUDFRONT_DIST_ID_* env variables defined for this to work
-aws cloudfront create-invalidation \
-  --distribution-id $CLOUDFRONT_DIST_ID \
-  --paths /*
+aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DIST_ID --paths /*
