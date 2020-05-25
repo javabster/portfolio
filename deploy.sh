f94292152bba8633c9e8034b49ed9749df6ddfc8
Abby Mitchell <abby.mitchell@btinternet.com>
2020-05-25 12:11:33 +0100

    Attempt to deploy backend to aws elastic beanstalk

--- deploy.sh
+++ deploy.sh
@@ -48,13 +48,13 @@ pip install --user --upgrade awsebcli
 
 # Configure AWS credentials for Elastic Beanstalk
 echo 'HEREEEEEEEEEEEEEEEEEEEEEEE'
-mkdir -p ./.aws
-echo '[profile eb-cli]' > ./.aws/config
-echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> ./.aws/config
-echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> ./.aws/config
+mkdir -p ~/.aws
+echo '[profile eb-cli]' > ~/.aws/config
+echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> ~/.aws/config
+echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> ~/.aws/config
 eb status $EB_ENV
 
 echo 'NOW HEREEEEEEEEEEEEEEEEEEEEEEE'
 # Deploy application to the appropriate ElasticBeanstalk env
 eb deploy $EB_ENV -v || exit 1
-rm ./.aws/config
+rm ~/.aws/config
