c0d36f673c64c836cbc593bb5fededd6fe06a591
Abby Mitchell <abby.mitchell@btinternet.com>
2020-05-30 12:18:27 +0100

    Attempt to connect frontend and backend in AWS

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors())
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+// app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/api/buttons/:lang', (req, res) => {
     var lang = req.params.lang;
@@ -149,9 +149,9 @@ app.get('/api/skills/:lang', (req, res) => {
 })
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+// app.get('*', (req,res) =>{
+//     res.sendFile(path.join(__dirname+'/client/build/index.html'));
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
