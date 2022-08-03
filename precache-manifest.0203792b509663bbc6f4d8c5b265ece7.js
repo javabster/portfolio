e403ac99a75f06ce32ca629d4caec0e56fef3eb1
Abby Mitchell <abby.s.mitchell@gmail.com>
2022-08-03 19:37:57 -0400

    Updates

---
+++ precache-manifest.0203792b509663bbc6f4d8c5b265ece7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987f0a25985e857adb8bef028f05c592",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9b1d6ccd912c2e609186",
+    "url": "/portfolio/static/css/main.ced62ef1.chunk.css"
+  },
+  {
+    "revision": "3a3ffd137cb5178a125b",
+    "url": "/portfolio/static/js/2.99696abe.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/portfolio/static/js/2.99696abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b1d6ccd912c2e609186",
+    "url": "/portfolio/static/js/main.13855c42.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/portfolio/static/js/main.13855c42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd220df2bc87e52323a",
+    "url": "/portfolio/static/js/runtime-main.41cc90cc.js"
+  },
+  {
+    "revision": "d95d3c0366bed65728b9968195c0ea11",
+    "url": "/portfolio/static/media/Aldrich-Regular.d95d3c03.ttf"
+  },
+  {
+    "revision": "359ed5a117466062d43ef1fdf7db6ce9",
+    "url": "/portfolio/static/media/cn-dark.359ed5a1.svg"
+  },
+  {
+    "revision": "0a5cdb7f59edbe6516216ebb78aa08e8",
+    "url": "/portfolio/static/media/down-arrow-dark.0a5cdb7f.svg"
+  },
+  {
+    "revision": "bebbfe7198052160b0968195017a5149",
+    "url": "/portfolio/static/media/down-arrow.bebbfe71.svg"
+  },
+  {
+    "revision": "299b25cd1c0296b602468e68d85c1012",
+    "url": "/portfolio/static/media/uk-dark.299b25cd.svg"
+  },
+  {
+    "revision": "22fed5eee79506bc447195aee8a5f6a1",
+    "url": "/portfolio/static/media/uk-light.22fed5ee.svg"
+  },
+  {
+    "revision": "d9e271d936ba6fdb489992d35196a362",
+    "url": "/portfolio/static/media/up-arrow-dark.d9e271d9.svg"
+  },
+  {
+    "revision": "67416519be6fb3fab5b9456db11c91f0",
+    "url": "/portfolio/static/media/up-arrow.67416519.svg"
+  }
+]);
